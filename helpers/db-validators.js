e395abcf433e2eef9f2441a24ee2b2efd3c74c92
Cristian Morales <cr15moresp@gmail.com>
2024-07-08 15:27:31 -0600

    (fix) Eliminado string inecesario en verificación de email existente

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -4,7 +4,7 @@ const emailExist = async(email = '') => {
    const emailDB = await InnovaUser.findOne({email});
 
    if (emailDB){
-      throw new Error(`El email ${email} ya está registrado en la base de datos, contacte a su administrador`);
+      throw new Error(`El email ${email} ya está registrado en la base de datos`);
    }
 }
 
